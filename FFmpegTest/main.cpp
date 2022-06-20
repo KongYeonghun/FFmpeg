e211412ecd884a69577a5d9a407ab6b2598792a7
KongYeonghun <yeonghunkong@gmail.com>
2022-06-20 11:25:39 +0900

    FFmpeg_220620

---
+++ FFmpegTest/main.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <tchar.h>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+#include <libavutil/avutil.h>
+}
+
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "swresample.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "avutil.lib")
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	av_log(NULL, AV_LOG_INFO, "FFFFFFFF\n");
+	return 0;
+}
